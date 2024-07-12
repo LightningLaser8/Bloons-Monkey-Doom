89fb08ffdeb89c70e10e34dcfd0e318d6a2dbc75
LightningLaser8 <LightningLaser8@users.noreply.github.com>
2024-07-12 17:54:34 +0100

    window size

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 850,
-    height: 850,
+    height: 870,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
